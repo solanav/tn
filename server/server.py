142a2ab5bc88cc2dcd68305e535923f4e915f95e
solanav <solanav@qq.com>
2020-06-03 18:15:56 +0200

    Server now saves data to log, no parsing and no complicated shit

--- server/server.py
+++ server/server.py
@@ -6,11 +6,14 @@ BUF_SIZE = 1024
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("127.0.0.1", PORT))
+    log = open("remote_log", "a")
 
     while 1:
         try:
             (data, addr) = s.recvfrom(BUF_SIZE)
-            print("[{}] > [{}]".format(addr, data))
+            msg = "[{}] > [{}]".format(addr, data)
+            print(msg)
+            log.write(msg)
         except Exception as e:
             print(e)
 
